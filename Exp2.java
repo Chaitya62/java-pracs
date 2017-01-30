8407de308e6a5ba685fda7de9cb75d985baa2e75
Chaitya Shah <Chaitya62@gmail.com>
2017-01-30 16:54:01 +0530

    Add question

--- Exp2.java
+++ Exp2.java
@@ -1,3 +1,14 @@
+/*
+
+
+Write a program which stores information about n players in a two dimensional array. The array should contain number of rows equal to number of players. Each row will have number of columns equal to number of matches played by that player which may vary from player to player. The program should display player number (index +1), runs scored in all matches and its batting average as output. (It is expected to assign columns to each row dynamically after getting value from user.Use scanner class to accept input from user.)
+
+
+*/
+
+
+
+
 import java.util.*;
 
 class Exp2{
